5ac64419dc5907b405e9e96f6bbb78745fdbce7f
TheWolfgri <qgrilletlycee@gmail.com>
2020-11-16 14:07:21 +0100

    Header+fonction add test menu

--- fonction.php
+++ fonction.php
@@ -1,1 +1,11 @@
+<?php
+function add_theme_scripts() {
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
 
+  }
+  add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+function register_my_menu() {
+register_nav_menu('header-menu',__( 'Header Menu' ));
+}
+add_action( 'init', 'register_my_menu' );
--- header.php
+++ header.php
@@ -1,15 +1,17 @@
 
 <html>
 <head>
-
+<?php wp_head() ?>
 <meta charset="utf-8">
 
 
 </head>
 
-<body>
+<body <?php body_class() ?>>
   <header>
-
+    <div class="nav">
+        <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+    </div>
 
 
   </header>
