4a4ecb83055a0307d50c53b4e864f4340d9b918c
TheWolfgri <qgrilletlycee@gmail.com>
2020-11-16 17:55:32 +0100

    end header
    begin home page css and html

--- front-page.php
+++ front-page.php
@@ -1,4 +1,41 @@
 <?php require('header.php');?>
+<section class="Sec_one">
+  <div>
+    <p><?php the_field('text_sect_one'); ?></p>
+  </div>
+  <div>
+    <img src="" alt="image">
+  </div>
+</section>
 
+<section class="missions">
+  <div class="ttt_mission">
+    <div class="t_mission">
+
+        <h1><?php the_field('mission'); ?></h1>
+
+
+        <?php the_field(''); ?>
+        <?php the_field(''); ?>
+
+
+    </div>
+    <div class="tt_mission">
+      <p><?php the_field('paragraphe_section_two'); ?></p>
+    </div>
+</div>
+</section>
+
+<section class="conferance">
+
+</section>
+
+<section class="Actu">
+
+</section>
+
+<section class="partner">
+
+</section>
 
 <?php require('footer.php');?>
--- header.php
+++ header.php
@@ -9,27 +9,29 @@
 $custom_logo_id = get_theme_mod( 'custom_logo' );
 $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
 ?>
-
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/header.css"></style>
 <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/reset.css"></style>
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/header.css"></style>
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/home.css"></style>
+
 
   <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
-  
+
 </head>
 
 <body <?php body_class() ?>>
 
   <header>
-    <div class="logo">
+    <div class="center_nav">
+      <div class="logo">
 
-              <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-                  <img src="<?php echo $image[0]; ?>" alt="Logo" class="logo">
-            </a>
-    </div>
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                    <img src="<?php echo $image[0]; ?>" alt="Logo" class="logo">
+              </a>
+      </div>
 
-    <div class="nav">
-        <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
-    </div>
-
+      <div class="nav">
+          <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+      </div>
+  </div>
 
   </header>
