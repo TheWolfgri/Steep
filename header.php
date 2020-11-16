9c374e616f0ab3ccd55856ef419022a31c7528cd
TheWolfgri <qgrilletlycee@gmail.com>
2020-11-16 14:58:15 +0100

    Add css on header: display inline+shadow_box, add reset css, modify function.php

--- header.php
+++ header.php
@@ -1,16 +1,20 @@
 
 <html>
-<head>
+<head <?php language_attributes(); ?>>
 <?php wp_head() ?>
 <meta charset="utf-8">
+<title><?php bloginfo('name');?> - <?php bloginfo('description');?></title>
 
 
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/header.css"></style>
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/reset.css"></style>
+  <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
 </head>
 
 <body <?php body_class() ?>>
   <header>
     <div class="logo">
-      
+
     </div>
     <div class="nav">
         <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
