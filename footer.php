5ed5d52fcdd33d96cfe15727085001695ae44f30
SHOKI15 <arthur.ferrod@gmail.com>
2020-11-17 16:29:56 +0100

    add line on tag line and add picto of footer

--- footer.php
+++ footer.php
@@ -1,25 +1,20 @@
-
-
-
-
-
-
-
 <footer >
   <img class="logo_footer"
     src="<?php echo get_template_directory_uri();?>/images/Logo_blanc.svg"
     alt="logo de steep en blanc">
 
     <div class="text1">
-      <a href="#"> Comprendre et Agir </a>
+<a href="#"><img src="<?php echo get_template_directory_uri();?>/images/comprendre_et_agir.svg" alt="logo comprendre et agir"></a>
     </div>
 
     <div class="nav_footer">
       <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
+    
     </div>
 
+
     <div class="tagline">
-      <p> Un site conçue par l'agence Bzzz </p>
+      <p>Un site conçue par l'agence Bzzz</p>
     </div>
 
 </footer>
