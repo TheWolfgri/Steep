00cc4fedbf5280c8b9bf0654a9ffb010227814ba
TheWolfgri <qgrilletlycee@gmail.com>
2020-11-18 12:00:46 +0100

    home.css+front-page add all title + correct wrap

--- front-page.php
+++ front-page.php
@@ -3,9 +3,12 @@
 
 
 <section class="Sec_one">
-  <div class="s1_first">
+  <div class="wrap">
+  <div class="Flex">
+
     <div class="s1_bloc_1">
-      <div class="wrap">
+
+        <div>
         <p><?php the_field('text_sect_one'); ?></p>
 
         <a href="" class="button_link_0">
@@ -13,10 +16,12 @@
         </a>
       </div>
 
-    </div>
-    <div class="s1_bloc_2">
-      <?php get_field('image_sect_one');?>
-      <img src="<?php the_field('image_sect_one'); ?>" class="image_so" />
+
+      </div>
+        <div class="s1_bloc_2">
+          <?php get_field('image_sect_one');?>
+          <img src="<?php the_field('image_sect_one'); ?>" class="image_so" />
+        </div>
     </div>
   </div>
 </section>
@@ -25,26 +30,33 @@
 <!--------------------------------------------------------------->
 
 <section class="missions">
+  <div class="content_mission">
+
   <div class="wrap">
 
-  <h1><?php the_field('mission'); ?></h1>
+    <h1><?php the_field('mission'); ?></h1>
 
-    <div class="t_mission">
+    <div class="Flex margin">
 
+      <div class="t_mission">
 
+        <div class="menu1">
+          <?php the_field('menu1'); ?>
+        </div>
+        <div class="menu2">
+          <?php the_field('menu2'); ?>
+        </div>
 
+      </div>
 
-        <?php the_field(''); ?>
-        <?php the_field(''); ?>
-
-
-    </div>
-
-    <div class="tt_mission">
-
-      <p><?php the_field('paragraphe_section_two'); ?></p>
-    </div>
+      <div class="tt_mission">
+        <p class="p1_mission"><?php the_field('paragraphe_section_two'); ?></p>
+        <p class="p2_mission" style="display: none;"><?php the_field('paragraphe_section_two_1'); ?></p>
+      </div>
   </div>
+      </div>
+</div>
+
 </section>
 
 
@@ -52,17 +64,21 @@
 
 <section class="conferance">
   <div class="wrap">
+    <h1><?php the_field('conferance_title'); ?></h1>
   </div>
 
 </section>
 <!--------------------------------------------------------------->
 <section class="Actu">
   <div class="wrap">
+    <h1><?php the_field('actu_title'); ?></h1>
   </div>
 </section>
 <!--------------------------------------------------------------->
 <section class="partner">
   <div class="wrap">
+
+  <h1><?php the_field('partenaire_title'); ?></h1>  
   </div>
 </section>
 <!--------------------------------------------------------------->
