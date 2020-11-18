1f767230714ea74d5979a56c499c36de2980280f
SHOKI15 <arthur.ferrod@gmail.com>
2020-11-18 11:57:11 +0100

    wrap on footer

--- footer.php
+++ footer.php
@@ -1,11 +1,13 @@
 <footer >
-  <img class="logo_footer"
-    src="<?php echo get_template_directory_uri();?>/images/Logo_blanc.svg"
-    alt="logo de steep en blanc">
+  <section class="wrap">
+      <div class="logo_footer">
+        <img src="<?php echo get_template_directory_uri();?>/images/Logo_blanc.svg"
+        alt="logo de steep en blanc">
+      </div>
 
-    <div class="text1">
-<a href="#"><img src="<?php echo get_template_directory_uri();?>/images/comprendre_et_agir.svg" alt="logo comprendre et agir"></a>
-    </div>
+      <div class="link">
+        <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/comprendre_et_agir.svg" alt="logo comprendre et agir"></a>
+      </div>
 
     <div class="nav_footer">
       <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
@@ -14,7 +16,7 @@
     <div class="tagline">
       <p>Un site conçue par l'agence Bzzz</p>
     </div>
-
+  </section>
 </footer>
 </body>
 </html>
