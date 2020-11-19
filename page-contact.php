9add5540df8e270e1808af06f538d6c2f2fdb7f8
SHOKI15 <arthur.ferrod@gmail.com>
2020-11-19 18:19:04 +0100

    fix section adress of page of contact

--- page-contact.php
+++ page-contact.php
@@ -4,72 +4,72 @@
 
 <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/contact-page.css">
   <div class="wrap">
+    <div class="all">
 
-<!-- adress -->
+    <!-- Titre contact-->
+    <section class="contact">
+      <div class="contact_title">
+        <img src="<?php the_field('contact_picto')?>" class="picto_title">
+        <h1><?php the_field('contact_title'); ?></h1>
+      </div>
 
-    <section class="adress">
+      <div class="contact_text">
+        <p><?php the_field('contact_text')?></p>
+      </div>
 
-        <div class="adress_title">
-          <img src="<?php the_field('contact_adresse_picto')?>" class="picto_title">
-          <h1><?php the_field('contact_adresse_title'); ?></h1>
+    <!-- info contact -->
+      <div class="mail_conf info_box" >
+          <img src="<?php the_field('contact_picto_1')?>" class="picto_info">
+        <div>
+          <p><?php the_field('contact_info_1')?></p>
+          <a  href="mailto:<?php the_field('contact_mail_1')?>"><?php the_field('contact_mail_1')?></a>
         </div>
+      </div>
 
-        <div class="adress_maps">
-          <img src="<?php the_field('contact_adresse_maps')?>" class="maps">
+      <div class="mail_team info_box">
+        <img src="<?php the_field('contact_picto_2')?>" class="picto_info">
+      <div>
+          <p><?php the_field('contact_info_2')?></p>
+          <a  href="mailto:<?php the_field('contact_mail_2')?>"><?php the_field('contact_mail_1')?></a>
         </div>
+      </div>
 
-        <div class="adress_text">
-          <p><?php the_field('contact_adresse_text')?></p>
+      <div class="join_team info_box">
+        <img src="<?php the_field('contact_picto_3')?>" class="picto_info">
+        <div>
+          <p><?php the_field('contact_info_3')?></p>
+          <a href="#"><?php the_field('contact_info_4')?></a>
         </div>
-
-    </section><!-- fin section adress -->
-
-<!-- contact -->
-
-    <div class="contact_title">
-      <img src="<?php the_field('contact_picto')?>" class="picto_title">
-      <h1><?php the_field('contact_title'); ?></h1>
-    </div>
-
-    <div class="contact_text">
-      <p><?php the_field('contact_text')?></p>
-    </div>
-
-  <section class="info">
-
-    <div class="mail_conf info_box" >
-      <img src="<?php the_field('contact_picto_1')?>" class="picto_info">
-      <div>
-        <p><?php the_field('contact_info_1')?></p>
-        <a  href="mailto:<?php the_field('contact_mail_1')?>"><?php the_field('contact_mail_1')?></a>
       </div>
-    </div>
 
-    <div class="mail_team info_box">
-      <img src="<?php the_field('contact_picto_2')?>" class="picto_info">
-      <div>
-        <p><?php the_field('contact_info_2')?></p>
-        <a  href="mailto:<?php the_field('contact_mail_2')?>"><?php the_field('contact_mail_1')?></a>
+      <div class="other info_box">
+        <img src="<?php the_field('contact_picto_4')?>" class="picto_info">
+        <div>
+          <p><?php the_field('contact_info_5')?></p>
+          <a  href="mailto:<?php the_field('contact_mail_3')?>"><?php the_field('contact_mail_1')?></a>
+          <a href="tel:<?php the_field('contact_num')?>"><?php the_field('contact_num')?></a>
+        </div>
       </div>
-    </div>
+  </section><!--fin section contact-->
 
-    <div class="join_team info_box">
-      <img src="<?php the_field('contact_picto_3')?>" class="picto_info">
-      <div>
-        <p><?php the_field('contact_info_3')?></p>
-        <a href="#"><?php the_field('contact_info_4')?></a>
+    <!-- info <address-->
+  <section class="adress">
+
+    </address>
+      <div class="adress_title">
+        <img src="<?php the_field('contact_adresse_picto')?>" class="picto_title">
+        <h1><?php the_field('contact_adresse_title'); ?></h1>
       </div>
-    </div>
 
-    <div class="other info_box">
-      <img src="<?php the_field('contact_picto_4')?>" class="picto_info">
-      <div>
-        <p><?php the_field('contact_info_5')?></p>
-        <a  href="mailto:<?php the_field('contact_mail_3')?>"><?php the_field('contact_mail_1')?></a>
-        <a href="tel:<?php the_field('contact_num')?>"><?php the_field('contact_num')?></a>
+      <div class="adress_maps">
+        <img src="<?php the_field('contact_adresse_maps')?>" class="maps">
       </div>
-    </div>
 
-  </section><!--fin section info_box -->
-  </div><!-- fin div wrap-->
+      <div class="adress_text">
+        <p><?php the_field('contact_adresse_text')?></p>
+      </div>
+
+  </section><!-- fin section adress -->
+</div><!--fin div all -->
+</div><!-- fin div wrap-->
 <?php require('footer.php');?>
