a27cae7b15561f7e456817477c1de96140958579
SHOKI15 <arthur.ferrod@gmail.com>
2020-11-19 12:57:11 +0100

    div contact not finish but bien avancé quand meme

--- page-contact.php
+++ page-contact.php
@@ -1,14 +1,13 @@
 <?php
-/* Template Name : Contact */?>
-<?php require('header.php');?>
+/* Template Name : Contact */
+ require('header.php');?>
 
 <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/contact-page.css">
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/home.css">
 <section class="contact_section">
   <div class="wrap">
 
     <div class="contact_title">
-      <img src="<?php the_field('contact_picto')?>" class="contact_picto">
+      <img src="<?php the_field('contact_picto')?>" class="picto_title">
       <h1><?php the_field('contact_title'); ?></h1>
     </div>
 
@@ -19,29 +18,40 @@
 
   <section class="info">
 
-      <div class="mail_conf">
-      <p><?php the_field('contact_info_1')?></p>
-      <a  href="mailto:<?php the_field('contact_mail_1')?>"><?php the_field('contact_mail_1')?></a>
+    <div class="mail_conf info_box" >
+      <img src="<?php the_field('contact_picto_1')?>" class="picto_info">
+      <div>
+        <p><?php the_field('contact_info_1')?></p>
+        <a  href="mailto:<?php the_field('contact_mail_1')?>"><?php the_field('contact_mail_1')?></a>
+      </div>
     </div>
 
-    <div class="mail_team">
-      <p><?php the_field('contact_info_2')?></p>
-      <a  href="mailto:<?php the_field('contact_mail_2')?>"><?php the_field('contact_mail_1')?></a>
+    <div class="mail_team info_box">
+      <img src="<?php the_field('contact_picto_2')?>" class="picto_info">
+      <div>
+        <p><?php the_field('contact_info_2')?></p>
+        <a  href="mailto:<?php the_field('contact_mail_2')?>"><?php the_field('contact_mail_1')?></a>
+      </div>
     </div>
 
-    <div class="join_team">
-      <p><?php the_field('contact_info_3')?></p>
-      <a href="#"><?php the_field('contact_info_4')?></a>
+    <div class="join_team info_box">
+      <img src="<?php the_field('contact_picto_3')?>" class="picto_info">
+      <div>
+        <p><?php the_field('contact_info_3')?></p>
+        <a href="#"><?php the_field('contact_info_4')?></a>
+      </div>
     </div>
 
-    <div class="other">
-      <p><?php the_field('contact_info_5')?></p>
-      <a  href="mailto:<?php the_field('contact_mail_3')?>"><?php the_field('contact_mail_1')?></a>
-      <a href="#"><?php the_field('contact_num')?>
+    <div class="other info_box">
+      <img src="<?php the_field('contact_picto_4')?>" class="picto_info">
+      <div>
+        <p><?php the_field('contact_info_5')?></p>
+        <a  href="mailto:<?php the_field('contact_mail_3')?>"><?php the_field('contact_mail_1')?></a>
+        <a href="#"><?php the_field('contact_num')?></a>
+      </div>
     </div>
-
-
 
 </section>
+    </div>
 </section>
 <?php require('footer.php');?>
